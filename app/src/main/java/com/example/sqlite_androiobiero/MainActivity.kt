e29a2b2f97c9e2cfd69f6b73884c09fc54ef521e
Klein254 <rayobiero62@gmail.com>
2023-04-17 13:11:40 +0300

    SQL Save Data

--- app/src/main/java/com/example/sqlite_androiobiero/MainActivity.kt
+++ app/src/main/java/com/example/sqlite_androiobiero/MainActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -34,15 +35,32 @@ class MainActivity : AppCompatActivity() {
         db = openOrCreateDatabase("ObieroDB", Context.MODE_PRIVATE, null)
 
         //CREATING A TABLE
-        db.execSQL("CREATE TABLE IF NOT EXISTS users")
+        db.execSQL("CREATE TABLE IF NOT EXISTS users(jina VARCHAR, arafa VARCHAR, kitambulisho VARCHAR)")
 
 
         btnSave.setOnClickListener {
+            var jina_edt = edtName.text.toString().trim()
+            var arafa_edt = edtEmail.text.toString().trim()
+            var kitambulisho_edt = edtID.text.toString().trim()
 
+            //VALIDATE EDIT TEXT
+            if(jina_edt.isEmpty() || kitambulisho_edt.isEmpty() || arafa_edt.isEmpty()){
+                Toast.makeText(this, "Cannot Submit an Empty Field", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                //INSERT DATA
+                db.execSQL("INSERT INTO users VALUES('\"+jina_edt+\"' , '\"+arafa_edt+\"' , '\"+kitambulisho_edt+\"')")
+
+                Toast.makeText(this, "Data Saved Successfully", Toast.LENGTH_SHORT).show()
+            }
         }
+
+
         btnView.setOnClickListener {
 
         }
+
+
         btnDelete.setOnClickListener {
 
         }
